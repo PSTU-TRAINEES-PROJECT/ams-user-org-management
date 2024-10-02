1ca2849edb46aeb6e5172dbd2f857bac760d8307
i~am~mim <mehedi.mim.bd@gmail.com>
2024-10-03 03:50:28 +0600

    Done all request

--- app/routers/api/v1/users_management/users.py
+++ app/routers/api/v1/users_management/users.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from utils.helpers.enums import Status
 from repository.database import get_db
 from repository.user_repository import UserRepository
 from services.users import UserService
-from sqlalchemy.ext.asyncio import  AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 
 user_router = APIRouter()
 repository = UserRepository()
@@ -11,13 +11,14 @@ user_service = UserService(repository)
 
 
 @user_router.get("/all-users")
-async def get_users(db: AsyncSession = Depends(get_db)):
+async def get_users(request: Request, db: AsyncSession = Depends(get_db)):
+    # Print incoming headers
+    print("Incoming Headers:")
+    for header, value in request.headers.items():
+        print(f"{header}: {value}")
     return await user_service.get_all_users(db)
-
 
 
 @user_router.post("/update-user-status/{user_id}")
 async def update_user_status(user_id: int, status: Status, db: AsyncSession = Depends(get_db)):
     return await user_service.update_user_status(user_id, status, db)
-
-
