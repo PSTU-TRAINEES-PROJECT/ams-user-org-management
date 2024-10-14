8880b3b830b89f59928468798e8e5fe98263b329
Rakibul73 <rakibul29185@yahoo.com>
2024-10-15 02:16:23 +0600

    Moved colors tag to a const file

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -2,33 +2,12 @@ from fastapi import Depends, Request
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from starlette.middleware.base import BaseHTTPMiddleware
 import logging
+from app.core.const import GREEN_BG, RESET
 
 logging.basicConfig(level=logging.INFO)
 
 class LogRequestPathMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # Foreground Colors
-        BLACK = "\033[30m"
-        RED = "\033[31m"
-        GREEN = "\033[32m"
-        YELLOW = "\033[33m"
-        BLUE = "\033[34m"
-        MAGENTA = "\033[35m"
-        WHITE = "\033[37m"
-        CYAN = "\033[96m"
-        
-        # Background Colors
-        BLACK_BG = "\033[40m"
-        RED_BG = "\033[41m"
-        GREEN_BG = "\033[42m"
-        YELLOW_BG = "\033[43m"
-        BLUE_BG = "\033[44m"
-        MAGENTA_BG = "\033[45m"
-        CYAN_BG = "\033[46m"
-        WHITE_BG = "\033[47m"
-        
-        RESET = "\033[0m"
-        
         logging.info(f"{GREEN_BG}Request path: {request.url.path}{RESET}")
         
         response = await call_next(request)
